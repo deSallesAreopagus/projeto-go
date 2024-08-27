10d96beec4108757fb7b39286b24c5e8db8a9f4b
Diego de Salles <diegosalles@areopagus.tech>
2024-08-27 18:58:48 -0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type book struct {
+	ID     string  `json:"id"`
+	Author string  `json:"author"`
+	Name   string  `json:"name"`
+	Price  float64 `json:"price"`
+}
+
+var books = []book{
+	{ID: "1", Author: "Autor 1", Name: "Livro 1", Price: 59.99},
+	{ID: "2", Author: "Autor 2", Name: "Livro 2", Price: 79.99},
+	{ID: "3", Author: "Autor 3", Name: "Livro 3", Price: 109.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+
+	router.Run("localhost:8000")
+}
+
+func getBooks(b *gin.Context) {
+	b.IndentedJSON(http.StatusOK, books)
+}
